c5ff955601441af49ed2be711bdb2f6b427e1e3d
Jacob Fowles <js.fowles@gmail.com>
2016-10-03 21:38:14 -0600

    react router setup

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { BrowserRouter, Match, Miss} from 'react-router';
+
 import './css/style.css';
 import App from './components/App';
-
 import StorePicker from './components/StorePicker';
+import NotFound from './components/NotFound';
 
- render(<App/>, document.getElementById("main"));
+const Root = () => {
+  return (
+    <BrowserRouter>
+      <div>
+        <Match exactly pattern='/' component={StorePicker} />
+        <Match pattern='/store/:storeID' component={App} />
+        <Miss component={NotFound} />
+
+      </div>
+    </BrowserRouter>
+  )
+}
+
+render(<Root/>, document.getElementById("main"));
