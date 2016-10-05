712b0b74b2b3b04352e50683cb8fd17ee3d5f84b
Jacob Fowles <js.fowles@gmail.com>
2016-10-05 10:07:36 -0600

    worked with events

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,11 +2,23 @@ import React from 'react';
 import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component{
+  // constructor() {
+  //   super();
+  //   this.goToStore = this.goToStore.bind(this);
+  // }
+  goToStore(event) {
+    event.preventDefault();
+    console.log('YouChanged the URL');
+  //first grab text
+  console.log(this.storeInput.value);
+  //second go to url
+  }
+
   render() {
     return (
-      <form className='store-selector'>
+      <form className='store-selector' onSubmit={this.goToStore.bind(this)}>
         <h2>Please Enter A Store</h2>
-        <input type='test' required placeholder="Store Name" defaultValue={getFunName()} />
+        <input type='test' required placeholder="Store Name" defaultValue={getFunName()} ref={(input) => { this.storeInput = input}}/>
         <button type='submit'>Visit Store</button>
       </form>
     )
