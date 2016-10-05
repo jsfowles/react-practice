a5b1f61c345c81a8f80ed7b8a6e21c1b557b6a14
Jacob Fowles <js.fowles@gmail.com>
2016-10-05 11:06:19 -0600

    playing with state

--- src/components/App.js
+++ src/components/App.js
@@ -4,17 +4,39 @@ import Order from './Order';
 import Inventory from './Inventory';
 
 class App extends React.Component {
-  render() {
-    return(
-      <div className='catch-of-the-day'>
-        <div className='menu'>
-          <Header tagline='Fresh Seafood Market'/>
-        </div>
-        <Order />
-        <Inventory />
-      </div>
-    )
-  }
+	constructor() {
+		super();
+
+    this.addFish = this.addFish.bind(this);
+		this.state = {
+			fishes: {},
+			order: {}
+		};
+	}
+
+	addFish(fish) {
+		//update our state
+		const fishes = {
+			...this.state.fishes
+		};
+		//add in our new fish
+		const timestamp = Date.now();
+		fishes[`fish-${timestamp}`] = fish;
+		//set state
+    this.setState({fishes});
+	}
+
+	render() {
+		return (
+			<div className='catch-of-the-day'>
+				<div className='menu'>
+					<Header tagline='Fresh Seafood Market'/>
+				</div>
+				<Order/>
+				<Inventory addFish={this.addFish}/>
+			</div>
+		)
+	}
 }
 
 export default App;
--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -7,18 +7,18 @@ class StorePicker extends React.Component{
   //   this.goToStore = this.goToStore.bind(this);
   // }
   goToStore(event) {
-    event.preventDefault();
+    event.preventDefault(event);
     console.log('YouChanged the URL');
   //first grab text
     const storeId = this.storeInput.value;
-    console.log('goin to ${storeID}');
+    console.log(`goin to ${storeId}`);
   //second go to url
-    this.context.router.transitionTo('/store/${storeID}')
+    this.context.router.transitionTo(`/store/${storeId}`)
   }
 
   render() {
     return (
-      <form className='store-selector' onSubmit={this.goToStore.bind(this)}>
+      <form className='store-selector' onSubmit={(e) => this.goToStore(e)}>
         <h2>Please Enter A Store</h2>
         <input type='test' required placeholder="Store Name" defaultValue={getFunName()} ref={(input) => { this.storeInput = input}}/>
         <button type='submit'>Visit Store</button>
